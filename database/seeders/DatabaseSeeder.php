f11712476e8c659ab452c4ad0aa2d0c9ce8c4302
Ashot1995 <ashotgharakeshishyan@gmail.com>
2022-01-13 02:14:35 +0400

    new crypto

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+//use database\seeds\UsersAndNotesSeeder;
+//use database\seeds\MenusTableSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            UsersAndNotesSeeder::class,
+            MenusTableSeeder::class,
+            FolderTableSeeder::class,
+            BREADSeeder::class,
+        ]);
+    }
+}
