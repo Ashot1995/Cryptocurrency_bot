4cb392244b473e0a997c3c714ab21a5641dcf846
multimontana <multitema@gmail.com>
2022-01-24 16:40:25 +0400

    fix

--- app/Helper/API3commas.php
+++ app/Helper/API3commas.php
@@ -6,7 +6,7 @@ namespace App\Helper;
 
 class API3commas
 {
-    public static function callAPI($method, $url1, $data) {
+    public static function callAPI($method, $url1, $data,$api_key='1523c6d6933641e38da8c09bef44ce34c219da76781447c8a6f393bb632fceab',$secret='8495c1825f16b0473b3b7a1dd712a889e0cb6b9e53001eda9e59d39808b65151b43f317f0ea6b5a57f54d01e19419678d7856a0260e168894579692ddc3c1ca8aa79167b9f46149c48acc52eb3ef08fff0115f716cf6ae24ab1b7a071fd19c23b006d0d7') {
         $url = 'https://api.3commas.io'.$url1;
         $curl = curl_init();
         switch ($method) {
