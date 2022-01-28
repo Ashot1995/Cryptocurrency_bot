7b013c5ccda933fa3bcf32b009881029b479d623
Uladzislau Danilchyk <danilchican@mail.ru>
2022-01-28 11:59:09 +0100

    Fixed api client

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -24,24 +24,31 @@ class StatisticsController extends Controller
 
 //        dd(json_decode(API3commas::callAPI('GET', '/public/api/ver1/bots', false)));
 
-        dd(json_decode(API3commas::callAPI('POST', '/public/api/ver1/bots/create_bot?' .
-            'name=ASH' .
-            '&type=binance' .
-            '&account_id=30587179' .
-            '&pairs=' .
-            '&base_order_volume=100' .
-            '&take_profit=' .
-            '&safety_order_volume=' .
-            '&martingale_volume_coefficient='.
-            '&martingale_step_coefficient='.
-            '&max_safety_orders=10'.
-            '&active_safety_orders_count='.
-            '&safety_order_step_percentage=' .
-            '&strategy_list=' .
-            '&take_profit_type=base', false)));
+        dd(json_decode(API3commas::execute('post', $this->prepareCreateBotRequestUri())));
 
         dd(json_decode(API3commas::callAPI('GET', '/public/api/ver1/bots?account_id=30587179', false)));
 
         return view('dashboard.statistic.statistic', compact('accounts'));
+    }
+
+    private function prepareCreateBotRequestUri(): string
+    {
+        return '/public/api/ver1/bots/create_bot?' . collect([
+            'name'                          => 'ASH',
+            'account_id'                    => 30587176,
+            'pairs'                         => urlencode("['BTC_LTC']"),
+            'base_order_volume'             => 100,
+            'take_profit'                   => 0.05,
+            'safety_order_volume'           => 0.1,
+            'martingale_volume_coefficient' => 1,
+            'martingale_step_coefficient'   => 1,
+            'max_safety_orders'             => 10,
+            'active_safety_orders_count'    => 50,
+            'safety_order_step_percentage'  => 5,
+            'take_profit_type'              => 'base',
+            'strategy_list'                 => urlencode(json_encode([['strategy' => 'manual']])),
+        ])->map(function ($value, $key) {
+            return $key . '=' . $value;
+        })->join('&');
     }
 }
